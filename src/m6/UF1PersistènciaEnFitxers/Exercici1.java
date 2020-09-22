6345a354c4419f3907865cedcb71a070aafc2c10
Alumne <Alumne@AI104-PC04>
2020-09-22 19:58:14 +0200

    Rename files

---
+++ src/m6/UF1PersistènciaEnFitxers/Exercici1.java
@@ -0,0 +1,24 @@
+package m6.UF1PersistènciaEnFitxers;
+import java.io.*;
+
+public class Exercici1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Fitxers al directori actual: ");
+        
+        // Creem l'objecte de tipus File per a obtindre tots els arxius de la carpeta on se executa el programa
+        File f = new File(".");
+        
+        // Usem el metpde list per a guardar els noms dels arxius a un array
+        String[] arxius = f.list();
+        
+        // Treiem per pantalla els noms dels arxius
+        for (int i = 0; i<arxius.length; i++){
+                System.out.println(arxius[i]);
+        }
+    }
+    
+}
