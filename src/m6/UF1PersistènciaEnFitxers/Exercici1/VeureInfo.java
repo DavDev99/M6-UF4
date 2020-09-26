1d8d3839ad3bed77cf6cc0ec453f15a57ea78596
DevDav99 <david.llop@insbaixcamp.cat>
2020-09-27 01:19:17 +0200

    Posar l'última data que s'ha modificat

--- src/m6/UF1PersistènciaEnFitxers/Exercici1/VeureInfo.java
+++ src/m6/UF1PersistènciaEnFitxers/Exercici1/VeureInfo.java
@@ -1,5 +1,7 @@
 package m6.UF1PersistènciaEnFitxers.Exercici1;
 import java.io.*;
+import java.time.LocalDate;
+import java.util.Date;
 
 public class VeureInfo {
 
@@ -9,7 +11,9 @@ public class VeureInfo {
     public static void main(String[] args) {
     	
     	File path = new File(args[0]);
-    	
+    	long threeDays = 259200000;
+    	long miliseconds;
+    	long today = System.currentTimeMillis() % 1000;
     	if (path.exists()) {
     		
     		if (path.isFile()) {
@@ -17,19 +21,39 @@ public class VeureInfo {
     			if (path.isHidden()) {
 					System.out.println("ES UN FITXER OCULT");
 				}
-    			
-    			 System.out.println("INFORMACIÓ SOBRE EL FITXER");
+    			miliseconds = path.lastModified();
 
-    				 System.out.println("Nom del fitxer : "+path.getName());
-    				 System.out.println("Ruta           : "+path.getPath());
-    				 System.out.println("Ruta absoluta  : "+path.getAbsolutePath());
-    				 System.out.println("Es pot escriure: "+path.canRead());
-    				 System.out.println("Es pot llegir  : "+path.canWrite());
-    				 System.out.println("Grandaria      : "+path.length());
-    				 System.out.println("Es un directori: "+path.isDirectory());
-    				 System.out.println("Es un fitxer   : "+path.isFile());
+    			Date lastModify = new Date(miliseconds);
+    			
+    			System.out.println("INFORMACIÓ SOBRE EL FITXER");
+    			
+    			System.out.println("ULTIMA MODIFICACIO DEL FITXER ES: " + lastModify);
+
+    			if (today - miliseconds <= threeDays) {
+        			System.out.println("EL FITXER S'HA MODIFICAT FA MENYS DE 3 DIES");
+				}
+
+				 System.out.println("Nom del fitxer : "+path.getName());
+				 System.out.println("Ruta           : "+path.getPath());
+				 System.out.println("Ruta absoluta  : "+path.getAbsolutePath());
+				 System.out.println("Es pot escriure: "+path.canRead());
+				 System.out.println("Es pot llegir  : "+path.canWrite());
+				 System.out.println("Grandaria      : "+path.length());
+				 System.out.println("Es un directori: "+path.isDirectory());
+				 System.out.println("Es un fitxer   : "+path.isFile());
 
 			} else if(path.isDirectory()) {
+				
+    			miliseconds = path.lastModified();
+
+    			Date lastModify = new Date(miliseconds);
+    			
+    			System.out.println("ULTIMA MODIFICACIO DEL DIRECTORY ES: " + lastModify);
+
+    			if (today - miliseconds <= threeDays) {
+        			System.out.println("EL DIRECTORY S'HA MODIFICAT FA MENYS DE 3 DIES");
+				}
+    			
 	            System.out.println("Fitxers al directori: ");	
 	            
 	            // Usem el metpde list per a guardar els noms dels arxius a un array
