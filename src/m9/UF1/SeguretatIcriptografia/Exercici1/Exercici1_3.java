e96016dad5e4439d4830234ff9158b269296478f
Alumne <Alumne@AI104-PC04>
2020-10-02 19:39:06 +0200

    cometari

--- src/m9/UF1/SeguretatIcriptografia/Exercici1/Exercici1_3.java
+++ src/m9/UF1/SeguretatIcriptografia/Exercici1/Exercici1_3.java
@@ -23,15 +23,14 @@ public class Exercici1_3 {
             frase = teclado.nextLine();
             
             
-            // Segons el numero de lletres que tingui la frase a desencriptar
+            // Agafem l'exercici de desencriptar i cambien el codi demanat per la i del primer for que es el numero posibilitats que hi ha
             for (int i = 0; i < numeroDeLletres; i++) {    
                 String descifrat = "";
                 for (int j = 0; j < frase.length(); j++) {
-                    // Si la lletra que estem desencriptan exixsteix al abecedari la desencriptem si no, posem la mateixa que ja llegit
+
                     int posicio = abecedari.indexOf(frase.charAt(j));
                     if (posicio >= 0) {
-                        // Conseguim la posicio de la lletra desencriptada pasant el numero de vegades que saltem al abecedari i restant este numero, 
-                        // i per a les lletres del principi com la a user el valor absolut de la resta
+
                         descifrat += (abecedari.charAt(Math.abs((posicio - i + numeroDeLletres) % numeroDeLletres)));
                     } else {
                         descifrat += frase.charAt(j);
