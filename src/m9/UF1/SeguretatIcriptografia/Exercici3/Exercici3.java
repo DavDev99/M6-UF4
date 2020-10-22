4d4cc2f8c336e5a5daa7342193a93f5ae8be322a
Alumne <Alumne@AI104-PC04>
2020-10-22 18:07:15 +0200

    Exercici 3 de M9 segona part

--- src/m9/UF1/SeguretatIcriptografia/Exercici3/Exercici3.java
+++ src/m9/UF1/SeguretatIcriptografia/Exercici3/Exercici3.java
@@ -34,11 +34,45 @@ public class Exercici3 {
         }
         return sKey;
     }
+    
+        public static SecretKey passwordKeyGenerationSHA384(String text, int keySize) throws UnsupportedEncodingException {
+        SecretKey sKey = null;
+        if ((keySize == 128) || (keySize == 192) || (keySize == 256)) {
+            try {
+                byte[] data = text.getBytes("UTF-8");
+                MessageDigest md = MessageDigest.getInstance("SHA-384");
+                byte[] hash = md.digest(data);
+                byte[] key = Arrays.copyOf(hash, keySize/8);
+                sKey = new SecretKeySpec(key, "AES");
+            } catch (Exception ex) {
+                System.err.println("Error generant la clau: " + ex);
+            }
+        }
+        return sKey;
+    }
+        
+        public static SecretKey passwordKeyGenerationSHA512224(String text, int keySize) throws UnsupportedEncodingException {
+        SecretKey sKey = null;
+        if ((keySize == 128) || (keySize == 192) || (keySize == 256)) {
+            try {
+                byte[] data = text.getBytes("UTF-8");
+                MessageDigest md = MessageDigest.getInstance("SHA-512/224");
+                byte[] hash = md.digest(data);
+                byte[] key = Arrays.copyOf(hash, keySize/8);
+                sKey = new SecretKeySpec(key, "AES");
+            } catch (Exception ex) {
+                System.err.println("Error generant la clau: " + ex);
+            }
+        }
+        return sKey;
+    }
 
     public static void main(String[] args) throws UnsupportedEncodingException {
 
         //Generem clau
-        SecretKey key1 = passwordKeyGeneration("aaaa", 256);
+        //SecretKey key1 = passwordKeyGeneration("aaaa", 128);
+        //SecretKey key1 = passwordKeyGenerationSHA384("aaaa", 128);
+        SecretKey key1 = passwordKeyGenerationSHA512224("aaaa", 128);
 
         //Agafem els bytes
         byte[] keyBytes = key1.getEncoded();
