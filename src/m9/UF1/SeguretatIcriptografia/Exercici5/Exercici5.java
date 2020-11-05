3d117175cb6d4702e8d31bc819e1a196eafa8992
Alumne <Alumne@AI104-PC04>
2020-11-05 19:11:45 +0100

    fent el 5 de m9

--- src/m9/UF1/SeguretatIcriptografia/Exercici5/Exercici5.java
+++ src/m9/UF1/SeguretatIcriptografia/Exercici5/Exercici5.java
@@ -9,18 +9,22 @@ import java.io.BufferedWriter;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 import java.io.Writer;
 import java.math.BigInteger;
 import java.security.InvalidKeyException;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Scanner;
 import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 
@@ -37,22 +41,27 @@ public class Exercici5 {
     public static void main(String[] args) throws Exception {
         // Variables
         Scanner teclado = new Scanner(System.in);
-        String text;
+        String textAEncriptar;
         String desxifrat;
-        String secure;
+        String texteEncriptat;
         String rutaClauPrivada;
         String rutaClauPublica;
 
         // Recollir dades
-        System.out.println("TEXT:");
-        text = teclado.next();
+        System.out.println("Text a encriptar:");
+        textAEncriptar = teclado.next();
         rutaClauPrivada = "/src/m9/UF1/SeguretatIcriptografia/Exercici5/clauPrivada.txt";
         rutaClauPublica = "/src/m9/UF1/SeguretatIcriptografia/Exercici5/clauPublica.txt";
 
+        // Crear i guardar claus
         genKeyPair(512);
-
         guardarClauPrivada(rutaClauPrivada);
         guardarClauPublica(rutaClauPublica);
+        
+        // Encriptar
+        
+        texteEncriptat = Encrypt(textAEncriptar);
+        System.out.println("Text encriptat:" + texteEncriptat );
 
     }
 
@@ -104,6 +113,18 @@ public class Exercici5 {
         b2[0] = 1;
         System.arraycopy(b, 0, b2, 1, b.length);
         return new BigInteger(b2).toString(36);
+    }
+    
+    public static String Encrypt(String plain) throws NoSuchAlgorithmException,NoSuchPaddingException, InvalidKeyException,IllegalBlockSizeException, BadPaddingException, InvalidKeySpecException, UnsupportedEncodingException, NoSuchProviderException {
+
+        byte[] encryptedBytes; 
+
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, clauPublica);
+        encryptedBytes = cipher.doFinal(plain.getBytes());
+
+        return bytesToString(encryptedBytes);
+
     }
 
 }
