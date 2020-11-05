1f44a2fc9835bfd557bab8eca2e9a20527a8bd24
Alumne <Alumne@AI104-PC04>
2020-11-05 18:09:02 +0100

    Començar el 5 de m9

--- src/m9/UF1/SeguretatIcriptografia/Exercici5/Exercici5.java
+++ src/m9/UF1/SeguretatIcriptografia/Exercici5/Exercici5.java
@@ -5,10 +5,57 @@
  */
 package m9.UF1.SeguretatIcriptografia.Exercici5;
 
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.Scanner;
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
 /**
  *
  * @author Alumne
  */
 public class Exercici5 {
-    
+
+    // Variables de clase
+
+    static PrivateKey privateKey;
+    static PublicKey publicKey;
+
+    public static void main(String[] args) throws Exception {
+        // Variables
+        Scanner teclado = new Scanner(System.in);
+        String text;
+
+        String clauPrivada;
+        String clauPublica;
+
+        // Recollir dades
+        System.out.println("TEXT:");
+        text = teclado.next();
+        
+        clauPrivada = "clauPrivada.txt";
+        clauPublica = "rclauPublica.txt";
+
+        genKeyPair(512);
+
+    }
+
+    public static void genKeyPair(int size) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+
+        KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+        kpg.initialize(size);
+        KeyPair kp = kpg.genKeyPair();
+
+        PublicKey publicKey2 = kp.getPublic();
+        PrivateKey privateKey2 = kp.getPrivate();
+
+        privateKey = privateKey2;
+        publicKey = publicKey2;
+    }
 }
