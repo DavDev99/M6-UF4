06959d1a3d02a7a1ef47ff6fb4735dc6ef024d2a
davi9922 <davi9922@users.noreply.github.com>
2020-11-08 18:18:04 +0100

    Exercici 6 m9 continuem la encriptació

--- src/m9/UF1/SeguretatIcriptografia/Exercici6/Encriptacio.java
+++ src/m9/UF1/SeguretatIcriptografia/Exercici6/Encriptacio.java
@@ -5,10 +5,59 @@
  */
 package m9.UF1.SeguretatIcriptografia.Exercici6;
 
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Base64;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+
 /**
  *
  * @author david
  */
 public class Encriptacio {
-    
+
+    private static SecretKeySpec secretKey;
+    private static byte[] key;
+
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException, InvalidKeyException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException {
+        Scanner teclado = new Scanner(System.in);
+        
+        // Nom de la clau publica
+        System.out.println("Introdueix el nom de la clau publica");
+        String nomFitxer = "src/m9/UF1/SeguretatIcriptografia/Exercici6/" + teclado.nextLine();
+        File clauPublicaFitxer = new File(nomFitxer);
+
+        // Si la clau exixsteix
+        if (clauPublicaFitxer.exists()) {
+            // Llegim els bytes
+            byte[] keyBytes = Files.readAllBytes(Paths.get(nomFitxer));
+            
+            // guardem la clau publica a una variable
+            X509EncodedKeySpec spec = new X509EncodedKeySpec(keyBytes);
+            KeyFactory kf = KeyFactory.getInstance("RSA");
+            PublicKey clauPublica = kf.generatePublic(spec);
+
+       
+
+        }
+
+    }
+
+  
 }
