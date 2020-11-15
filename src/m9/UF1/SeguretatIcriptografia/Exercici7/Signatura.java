eec4fa8f47b8ec7ada521b8ca7f0abca42d745bf
DevDav99 <david.llop@insbaixcamp.cat>
2020-11-15 17:59:27 +0100

    Creant la firma i guardantla

--- src/m9/UF1/SeguretatIcriptografia/Exercici7/Signatura.java
+++ src/m9/UF1/SeguretatIcriptografia/Exercici7/Signatura.java
@@ -5,23 +5,28 @@
  */
 package m9.UF1.SeguretatIcriptografia.Exercici7;
 
+import java.io.DataOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-
+import java.io.UnsupportedEncodingException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
-
+import java.security.Key;
+import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
-
+import java.security.Signature;
 import java.security.SignatureException;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
-
+import java.util.Base64;
+import java.util.Scanner;
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
@@ -67,5 +72,48 @@ public class Signatura {
         }
 
         System.out.println("OK");
+        
+        // Declarem variables
+        Scanner teclado = new Scanner(System.in);
+        String missatge;
+
+        // Guardem el missatge a firmar
+        System.out.println("Introdueix el missatge a signar:");
+        missatge = teclado.nextLine();
+        System.out.print("Signant el missatge...");
+        
+        // Generem firma i firmem missatge
+        Signature privateSignature = Signature.getInstance("SHA256withRSA");
+        privateSignature.initSign(clauPrivada);
+        privateSignature.update(missatge.getBytes("UTF8"));
+        System.out.println("OK");
+
+        byte[] firma = privateSignature.sign();
+
+        // Guardem la firma a un fitxer
+        System.out.print("Generant arxiu firma_missatge");
+        
+        try {
+            FileOutputStream fos = new FileOutputStream("src/m9/UF1/SeguretatIcriptografia/Exercici7/firma_missatge.txt");
+            fos.write(firma);
+            fos.close();
+        } catch (Exception e) {
+            System.out.println("ERROR guardant la firma");
+        }
+        System.out.println("...OK");
+        
+        
+        System.out.print("Generant arxiu missatge");
+        try{
+            FileOutputStream fos = new FileOutputStream("src/m9/UF1/SeguretatIcriptografia/Exercici7/missatge.txt");
+            fos.write(missatge.getBytes());
+            fos.close();
+        } catch (Exception e) {
+            System.out.println("ERROR guardant el missatge");
+        }        
+
+        System.out.println("...OK");
+
     }
+
 }
