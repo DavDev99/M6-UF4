76161892641da98a9eef5a2ccfbc7f4ba0401c4f
davi9922 <davi9922@users.noreply.github.com>
2020-11-22 20:55:45 +0100

    Inserir alumnes ok

--- src/m6/UF2PersistenciaenBDBDORiBDOO/Exercici1/Exercici1.java
+++ src/m6/UF2PersistenciaenBDBDORiBDOO/Exercici1/Exercici1.java
@@ -8,7 +8,9 @@ package m6.UF2PersistenciaenBDBDORiBDOO.Exercici1;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 /**
  *
@@ -16,35 +18,97 @@ import java.sql.Statement;
  */
 public class Exercici1 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+        Scanner teclado = new Scanner(System.in);
+        int opcio = -1;
+        String nom;
+        String dni;
+        String dataNaixement;
+        String adresaPostal;
+        String sexe;
+        int codiPostal;
+        String poblacio;
+
         Connection connection = null;
-        Statement insertStmt = null;
-        Statement selectStmt = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager
+                    .getConnection("jdbc:mysql://localhost:3306/exercici1_m6", "root", "");
+        } finally {
+            try {
+                connection.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        // Bucle principal del menu
+        while (opcio != 0) {
+            System.out.println("Que vols fer? \n"
+                    + "1. Crear un alumne \n"
+                    + "2. Modificar alumne \n"
+                    + "3. Eliminar alumne \n"
+                    + "0. sortir \n");
+
+            opcio = teclado.nextInt();
+            teclado.nextLine();
+
+            // Creacio del alumne
+            if (opcio == 1) {
+                System.out.println("Introdueix un nom");
+                nom = teclado.nextLine();
+
+                System.out.println("Introdueix un DNI");
+                dni = teclado.nextLine();
+
+                System.out.println("Introdueix data de naixement (exemple: 1999-03-22)");
+                dataNaixement = teclado.nextLine();
+
+                System.out.println("Introdueix adreça postal");
+                adresaPostal = teclado.nextLine();
+
+                System.out.println("Introdueix sexe");
+                sexe = teclado.nextLine();
+
+                System.out.println("Introdueix codi postal");
+                codiPostal = teclado.nextInt();
+                teclado.nextLine();
+
+                System.out.println("Introdueix poblacio");
+                poblacio = teclado.nextLine();
+
+                inseriAlumnes(nom, dni, dataNaixement, adresaPostal, sexe, codiPostal, poblacio);
+
+            } else if (opcio == 2) {
+                // Modificar alumne
+
+            }
+        }
+
+    }
+
+    private static void inseriAlumnes(String nom, String dni, String dataNaixement, String adresaPostal, String sexe, int codiPostal, String poblacio) {
+
+        Statement stmt = null;
+        Connection connection = null;
         try {
             Class.forName("com.mysql.jdbc.Driver");
             connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/exercici1_m6", "root", "");
 
-            /*insertStmt = connection.createStatement();
-            insertStmt.execute("INSERT INTO EMPLOYEE (ID,FIRST_NAME,LAST_NAME,STAT_CD) VALUES (1,'Lokesh','Gupta',5)");
-            insertStmt.execute("INSERT INTO EMPLOYEE (ID,FIRST_NAME,LAST_NAME,STAT_CD) VALUES (2,'howtodoinjava','com',5)");*/
-            selectStmt = connection.createStatement();
-            ResultSet rs = selectStmt.executeQuery("SELECT ID,nom FROM alumnes");
-            
-            while (rs.next()) {
-                System.out.println(rs.getString(1));    //First Column
-                System.out.println(rs.getString(2));    //Second Column
-            }
+            stmt = connection.createStatement();
+            stmt.execute("INSERT INTO alumnes (nom, DNI, data_naixement, adreça_postal, sexe, codi_postal, poblacio) VALUES ('" + nom + "','" + dni + "','" + dataNaixement + "','" + adresaPostal + "','" + sexe + "'," + codiPostal + ",'" + poblacio + "')");
             
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             try {
-                selectStmt.close();
-                insertStmt.close();
+                stmt.close();
                 connection.close();
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
     }
+    
+
 }
