7c3a173217ac33b4d602761ace60ebe054152c15
davi9922 <davi9922@users.noreply.github.com>
2020-11-22 20:24:02 +0100

    provant la base de dades

--- src/m6/UF2PersistenciaenBDBDORiBDOO/Exercici1/Exercici1.java
+++ src/m6/UF2PersistenciaenBDBDORiBDOO/Exercici1/Exercici1.java
@@ -5,10 +5,46 @@
  */
 package m6.UF2PersistenciaenBDBDORiBDOO.Exercici1;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 /**
  *
  * @author david
  */
 public class Exercici1 {
-    
+
+    public static void main(String[] args) {
+        Connection connection = null;
+        Statement insertStmt = null;
+        Statement selectStmt = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/exercici1_m6", "root", "");
+
+            /*insertStmt = connection.createStatement();
+            insertStmt.execute("INSERT INTO EMPLOYEE (ID,FIRST_NAME,LAST_NAME,STAT_CD) VALUES (1,'Lokesh','Gupta',5)");
+            insertStmt.execute("INSERT INTO EMPLOYEE (ID,FIRST_NAME,LAST_NAME,STAT_CD) VALUES (2,'howtodoinjava','com',5)");*/
+            selectStmt = connection.createStatement();
+            ResultSet rs = selectStmt.executeQuery("SELECT ID,nom FROM alumnes");
+            
+            while (rs.next()) {
+                System.out.println(rs.getString(1));    //First Column
+                System.out.println(rs.getString(2));    //Second Column
+            }
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                selectStmt.close();
+                insertStmt.close();
+                connection.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
