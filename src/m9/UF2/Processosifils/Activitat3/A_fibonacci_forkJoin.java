81415ba3a0f06a639d85c4a17f3d8dcebb83eca0
Alumne <Alumne@AI104-PC04>
2021-01-14 16:17:04 +0100

    exercici 3

--- src/m9/UF2/Processosifils/Activitat3/A_fibonacci_forkJoin.java
+++ src/m9/UF2/Processosifils/Activitat3/A_fibonacci_forkJoin.java
@@ -13,23 +13,28 @@ import java.util.concurrent.RecursiveTask;
  * @author Alumne
  */
 public class A_fibonacci_forkJoin extends RecursiveTask<Long> {
+
     long numero;
-    public A_fibonacci_forkJoin(long numero){
-        this.numero=numero;
+
+    public A_fibonacci_forkJoin(long numero) {
+        this.numero = numero;
     }
-    
+
     @Override
     protected Long compute() {
         // ATENCIO **1** double calcul = java.lang.Math.cos(54879854);
-        if(numero <= 1) return numero;
-        A_fibonacci_forkJoin fib1 = new A_fibonacci_forkJoin(numero-1);
+        if (numero <= 1) {
+            return numero;
+        }
+        A_fibonacci_forkJoin fib1 = new A_fibonacci_forkJoin(numero - 1);
         //fib1.fork();
-	A_fibonacci_forkJoin fib2 = new A_fibonacci_forkJoin(numero-2);
+        A_fibonacci_forkJoin fib2 = new A_fibonacci_forkJoin(numero - 2);
         fib2.fork();
-	 return fib1.compute()+ fib2.join();
-	 }
-    public static void main(String[] args){
+        return fib1.compute() + fib2.join();
+    }
+
+    public static void main(String[] args) {
         ForkJoinPool pool = new ForkJoinPool();
-        System.out.println("Calculat:  " + pool.invoke(new A_fibonacci_forkJoin(35)));    
+        System.out.println("Calculat:  " + pool.invoke(new A_fibonacci_forkJoin(35)));
     }
 }
--- src/m9/UF2/Processosifils/Activitat3/Activitat3FibonacciRecursiu.java
+++ src/m9/UF2/Processosifils/Activitat3/Activitat3FibonacciRecursiu.java
@@ -5,10 +5,29 @@
  */
 package m9.UF2.Processosifils.Activitat3;
 
+import java.util.Scanner;
+import java.util.concurrent.ForkJoinPool;
+
 /**
  *
  * @author Alumne
  */
 public class Activitat3FibonacciRecursiu {
-    
+
+    public static void main(String[] args) {
+        calculaFibonacci(35);
+    }
+
+    public static long calculaFibonacci(long numero) {
+        // Calcul dificil
+        double calcul = java.lang.Math.cos(54879854);
+        
+        if (numero == 0) {
+            return 0;
+        } else if (numero == 1) {
+            return 1;
+        } else {
+            return (calculaFibonacci(numero - 2) + calculaFibonacci(numero - 1));
+        }
+    }
 }
