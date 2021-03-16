43ef467d4aea09ce98b4b8193ab53f36b9eb8290
davi9922 <55253543+davi9922@users.noreply.github.com>
2021-03-16 15:53:24 +0100

    Activitat3

---
+++ src/m6/UF3PersistènciaenBDnativesXML/Exercici3/Activitat3.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m6.UF3PersistènciaenBDnativesXML.Exercici3;
+
+/**
+ *
+ * @author David
+ */
+public class Activitat3 {
+    
+}
