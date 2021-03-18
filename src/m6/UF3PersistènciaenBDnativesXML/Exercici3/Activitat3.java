fbddaae55f4e64dccfef304ebea42edd08392a7c
davi9922 <55253543+davi9922@users.noreply.github.com>
2021-03-18 17:32:43 +0100

    Activitat3 crea i modifica

--- src/m6/UF3PersistènciaenBDnativesXML/Exercici3/Activitat3.java
+++ src/m6/UF3PersistènciaenBDnativesXML/Exercici3/Activitat3.java
@@ -18,17 +18,18 @@ import org.xmldb.api.modules.*;
  */
 public class Activitat3 {
 
+    //Driver per a eXist
+    static String driver = "org.exist.xmldb.DatabaseImpl";
+    //Col·lecció
+    static Collection col = null;
+    //URI col·lecció
+    static String URI = "xmldb:exist://localhost:8080/exist/xmlrpc/db/";
+    //Usuari
+    static String usu = "admin";
+    //Contrasenya
+    static String usuPass = "admin";
+
     public static void main(String[] args) throws XMLDBException {
-        //Driver per a eXist
-        String driver = "org.exist.xmldb.DatabaseImpl";
-        //Col·lecció
-        Collection col = null;
-        //URI col·lecció
-        String URI = "xmldb:exist://localhost:8080/exist/xmlrpc/db/";
-        //Usuari
-        String usu = "admin";
-        //Contrasenya
-        String usuPass = "admin";
 
         try {
             //Carrega el driver
@@ -59,10 +60,8 @@ public class Activitat3 {
         if (col == null) {
             System.out.println("*** LA COL·LECCIÓ NO EXISTEIX ***");
         }
-        XPathQueryService servei
-                = (XPathQueryService) col.getService("XPathQueryService", "1.0");
-        ResourceSet result
-                = servei.query("for $em in /EMPLEADOS/EMP_ROW[DEPT_NO=" + departament + "] return $em");
+        XPathQueryService servei = (XPathQueryService) col.getService("XPathQueryService", "1.0");
+        ResourceSet result = servei.query("for $em in /EMPLEADOS/EMP_ROW[DEPT_NO=" + departament + "] return $em");
 
         //Recórrer les dades del recurs
         ResourceIterator i;
@@ -76,9 +75,109 @@ public class Activitat3 {
 
         }
         //S'esborra
+
+//        insereixdep();        
+        modificardep();
+
         col.close();
 
     }
 
-}
+    public static void insereixdep() throws XMLDBException {
+        int numDep = 0;
+        String nameDep = "";
+        String localitatDep = "";
 
+        try {
+            System.out.println("Escriu un codi departament:");
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+            numDep = Integer.parseInt(in.readLine());
+
+            System.out.println("Escriu un nom de departament:");
+
+            in = new BufferedReader(new InputStreamReader(System.in));
+            nameDep = in.readLine();
+
+            System.out.println("Escriu una localitat per al departament:");
+
+            in = new BufferedReader(new InputStreamReader(System.in));
+            localitatDep = in.readLine();
+
+        } catch (IOException e) {
+            System.out.println("Error en llegir");
+            e.printStackTrace();
+        }
+
+        col = DatabaseManager.getCollection(URI, usu, usuPass);
+        if (col == null) {
+            System.out.println("*** LA COL·LECCIÓ NO EXISTEIX ***");
+        }
+        XPathQueryService servei = (XPathQueryService) col.getService("XPathQueryService", "1.0");
+        ResourceSet result = servei.query("update insert <DEP_ROW>"
+                + "        <DEPT_NO>" + numDep + "</DEPT_NO>"
+                + "        <DNOMBRE>" + nameDep + "</DNOMBRE>"
+                + "        <LOC>" + localitatDep + "</LOC>"
+                + "    </DEP_ROW> into /departamentos");
+
+    }
+
+    public static void modificardep() throws XMLDBException {
+        int numDep = 0;
+        int numToChange = 0;
+        String nameDep = "";
+        String localitatDep = "";
+        col = DatabaseManager.getCollection(URI, usu, usuPass);
+        XPathQueryService servei = (XPathQueryService) col.getService("XPathQueryService", "1.0");
+
+        try {
+            System.out.println("Escriu el departament a modificar:");
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+            numToChange = Integer.parseInt(in.readLine());
+
+        } catch (IOException e) {
+            System.out.println("Error en llegir");
+            e.printStackTrace();
+        }
+
+        ResourceSet result = servei.query("/departamentos/DEP_ROW[number(DEPT_NO) = " + numDep + "]");
+        //Recórrer les dades del recurs
+        ResourceIterator i;
+        i = result.getIterator();
+        
+        if (!i.hasMoreResources()) {
+            
+            System.out.println("LA CONSULTA NO TORNA RES");
+            
+        } else {
+            
+            try {
+                System.out.println("Escriu un codi departament:");
+                BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+                numDep = Integer.parseInt(in.readLine());
+
+                System.out.println("Escriu un nom de departament:");
+
+                in = new BufferedReader(new InputStreamReader(System.in));
+                nameDep = in.readLine();
+
+                System.out.println("Escriu una localitat per al departament:");
+
+                in = new BufferedReader(new InputStreamReader(System.in));
+                localitatDep = in.readLine();
+
+            } catch (IOException e) {
+                System.out.println("Error en llegir");
+                e.printStackTrace();
+            }
+
+            result = servei.query("update replace /departamentos/DEP_ROW[number(DEPT_NO) = " + numToChange + "] with "
+                    + "<DEP_ROW>"
+                    + "   <DEPT_NO>" + numDep + "</DEPT_NO>"
+                    + "   <DNOMBRE>" + nameDep + "</DNOMBRE>"
+                    + "   <LOC>" + localitatDep + "</LOC>"
+                    + "</DEP_ROW> into /departamentos");
+        }
+
+    }
+
+}
